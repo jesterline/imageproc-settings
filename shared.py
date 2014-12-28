6191054b6a05f4a3ec0d40d0c4e6774cb28d87d7
Ron Fearing <ronf@eecs.berkeley.edu>
2014-12-27 16:12:29 -0800

    shared settings initial commit

---
+++ shared.py
@@ -0,0 +1,14 @@
+BS_COMPORT = 'COM4' # ron
+#BS_COMPORT = 'COM3' # duncan, ROBOPROC
+BS_BAUDRATE = 230400
+
+motor_gains_set = False
+steering_gains_set = False
+steering_rate_set = False
+pkts = 0
+bytesIn = 0
+count2deg = 2000.0/(2**15-1)
+
+imudata = [[]] * 300 # HACK should use numSamples
+statedata = []
+dutycycles = []
